8d2319ecd5edb088a8bf9ff9892ec7aed3227916
Albrecht Ohsiek <albrecht.ohsiek@eps.ch>
2023-11-20 20:38:21 +0200

    Login

--- toucan/lib/ui/pages/login/login.dart
+++ toucan/lib/ui/pages/login/login.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart' as http;
+import 'package:toucan/ui/pages/order/order.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -9,6 +14,10 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   bool isSignIn = true;
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,12 +35,21 @@ class _AuthScreenState extends State<AuthScreen> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 125,
+                child: SvgPicture.asset(
+                  '/images/svg/logo.svg',
+                  colorFilter:
+                      const ColorFilter.mode(Colors.black, BlendMode.srcIn),
+                ),
+              ),
               const SizedBox(height: 40),
               isSignIn ? buildSignInForm() : buildRegisterForm(),
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
-                  // TODO: Implement your authentication logic here
+                  authenticate();
                 },
                 child: Text(isSignIn ? 'Sign In' : 'Register'),
               ),
@@ -54,41 +72,97 @@ class _AuthScreenState extends State<AuthScreen> {
   }
 
   Widget buildSignInForm() {
-    return const Column(
+    return Column(
       children: [
         TextField(
-          decoration: InputDecoration(labelText: 'Username'),
+          decoration: const InputDecoration(labelText: 'Username'),
+          controller: usernameController,
         ),
-        SizedBox(height: 10),
+        const SizedBox(height: 10),
         TextField(
           obscureText: true,
-          decoration: InputDecoration(labelText: 'Password'),
+          decoration: const InputDecoration(labelText: 'Password'),
+          controller: passwordController,
         ),
       ],
     );
   }
 
   Widget buildRegisterForm() {
-    return const Column(
+    return Column(
       children: [
         TextField(
-          decoration: InputDecoration(labelText: 'Username'),
+          decoration: const InputDecoration(labelText: 'Username'),
+          controller: usernameController,
         ),
-        SizedBox(height: 10),
+        const SizedBox(height: 10),
         TextField(
-          decoration: InputDecoration(labelText: 'Email'),
+          decoration: const InputDecoration(labelText: 'Email'),
+          controller: emailController,
         ),
-        SizedBox(height: 10),
-        TextField(
-          obscureText: true,
-          decoration: InputDecoration(labelText: 'Password'),
-        ),
-        SizedBox(height: 10),
+        const SizedBox(height: 10),
         TextField(
           obscureText: true,
-          decoration: InputDecoration(labelText: 'Confirm Password'),
+          decoration: const InputDecoration(labelText: 'Password'),
+          controller: passwordController,
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          obscureText: true,
+          decoration: const InputDecoration(labelText: 'Confirm Password'),
+          controller: confirmPasswordController,
         ),
       ],
+    );
+  }
+
+  Future<void> authenticate() async {
+    const String baseUrl = 'http://35.205.19.65:4000/';
+
+    final String endpoint = isSignIn ? 'api/users/login' : 'api/users/register';
+
+    final Map<String, String> data = {
+      'name': usernameController.text,
+      'password': passwordController.text,
+    };
+    if (!isSignIn) {
+      data['email'] = emailController.text;
+      data['confirmPassword'] = confirmPasswordController.text;
+    }
+
+    final client = http.Client();
+
+    try {
+      final response = await client.post(
+        Uri.parse('$baseUrl$endpoint'),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode == 200) {
+        redirect();
+        print('Authentication successful');
+      } else if (response.statusCode == 400) {
+        print('Bad Request. Response body: ${response.body}');
+      } else {
+        print('Authentication failed, ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error during authentication: $e');
+    } finally {
+      client.close();
+    }
+  }
+
+  void redirect() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) =>
+            const OrderScreen(), // Replace 'NewPage' with the actual widget for the new page
+      ),
     );
   }
 }
